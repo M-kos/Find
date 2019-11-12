b9f23c3bf30f3aa41eea118536174daa709f0cb0
Konstantin Melnikov <koc.melnikov.1985@gmail.com>
2019-11-12 19:51:15 +0300

    Function for searching substrings in text created

--- src/App.js
+++ src/App.js
@@ -7,23 +7,41 @@ import TextBlock from './components/textBlock/textBlock';
 class App extends Component {
 
   state = {
-    inputValue: ''
+    inputValue: '',
+    originalText: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia quas odio dolores? Id corporis ad aspernatur alias nesciunt quas, asperiores in sit tempore itaque ipsam debitis fuga dolores illum! Eaque, atque id! Accusamus vitae corporis cum architecto sed eius excepturi atque. Accusamus quae nemo harum aspernatur iure molestiae facere repudiandae. Sint sapiente, officia consectetur praesentium incidunt autem necessitatibus! Impedit officiis eligendi autem porro at, quia doloremque laudantium rem! Aliquid quibusdam repellendus quis assumenda aspernatur. Quae voluptatem quaerat nesciunt asperiores, expedita quam necessitatibus magni optio eveniet sapiente eos eligendi. Molestiae minus minima eos amet soluta voluptatibus unde tempore quae. Officia, obcaecati.Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia quas odio dolores? Id corporis ad aspernatur alias nesciunt quas, asperiores in sit tempore itaque ipsam debitis fuga dolores illum! Eaque, atque id! Accusamus vitae corporis cum architecto sed eius excepturi atque. Accusamus quae nemo harum aspernatur iure molestiae facere repudiandae. Sint sapiente, officia consectetur praesentium incidunt autem necessitatibus! Impedit officiis eligendi autem porro at, quia doloremque laudantium rem! Aliquid quibusdam repellendus quis assumenda aspernatur. Quae voluptatem quaerat nesciunt asperiores, expedita quam necessitatibus magni optio eveniet sapiente eos eligendi. Molestiae minus minima eos amet soluta voluptatibus unde tempore quae. Officia, obcaecati.',
+    outputText: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia quas odio dolores? Id corporis ad aspernatur alias nesciunt quas, asperiores in sit tempore itaque ipsam debitis fuga dolores illum! Eaque, atque id! Accusamus vitae corporis cum architecto sed eius excepturi atque. Accusamus quae nemo harum aspernatur iure molestiae facere repudiandae. Sint sapiente, officia consectetur praesentium incidunt autem necessitatibus! Impedit officiis eligendi autem porro at, quia doloremque laudantium rem! Aliquid quibusdam repellendus quis assumenda aspernatur. Quae voluptatem quaerat nesciunt asperiores, expedita quam necessitatibus magni optio eveniet sapiente eos eligendi. Molestiae minus minima eos amet soluta voluptatibus unde tempore quae. Officia, obcaecati.Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia quas odio dolores? Id corporis ad aspernatur alias nesciunt quas, asperiores in sit tempore itaque ipsam debitis fuga dolores illum! Eaque, atque id! Accusamus vitae corporis cum architecto sed eius excepturi atque. Accusamus quae nemo harum aspernatur iure molestiae facere repudiandae. Sint sapiente, officia consectetur praesentium incidunt autem necessitatibus! Impedit officiis eligendi autem porro at, quia doloremque laudantium rem! Aliquid quibusdam repellendus quis assumenda aspernatur. Quae voluptatem quaerat nesciunt asperiores, expedita quam necessitatibus magni optio eveniet sapiente eos eligendi. Molestiae minus minima eos amet soluta voluptatibus unde tempore quae. Officia, obcaecati.'
   };
 
   valueChange = (e) => {
+    console.log(e.target.value);
+    
     this.setState(
-      {
-        inputValue: e.target.value
-      });
+      { inputValue: e.target.value },
+      this.searchSubStr
+    );
+  };
+
+  searchSubStr = () => {
+    const { originalText } = this.state;
+
+    let strInputValRep = this.state.inputValue.replace(' ', '|')
+
+    let rExp = new RegExp(strInputValRep, "gi");
+
+    let subOutputText = originalText.split(' ');
+    let arrText = subOutputText.map((el) => {
+      return el.replace(rExp, (match) => `<span>${match}</span>`);
+    });
+    this.setState({outputText: arrText.join(' ')});
   };
 
   render() {
-    const { inputValue } = this.state;
+    const { inputValue, outputText } = this.state;
 
     return (
       <div className="App">
         <SearchLine valueChange = {this.valueChange} value = {inputValue} />
-        <TextBlock />
+        <TextBlock outputText={outputText} />
       </div>
     );
   }
