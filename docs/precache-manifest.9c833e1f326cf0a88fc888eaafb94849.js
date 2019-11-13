bbd5ce47cedc7be6e527d91dfb43aaf44d1df30b
Konstantin Melnikov <koc.melnikov.1985@gmail.com>
2019-11-13 10:58:11 +0300

    docs

---
+++ docs/precache-manifest.9c833e1f326cf0a88fc888eaafb94849.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed803da617cbc5e43a815c34265a9b2e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "804dbcb1ae7f2ebeddcc",
+    "url": "/static/css/main.87ffaf65.chunk.css"
+  },
+  {
+    "revision": "6e6cc9b486bf9a5b73e1",
+    "url": "/static/js/2.dcc45ffe.chunk.js"
+  },
+  {
+    "revision": "804dbcb1ae7f2ebeddcc",
+    "url": "/static/js/main.f77cd6b7.chunk.js"
+  },
+  {
+    "revision": "b99cd67774f3e227f334",
+    "url": "/static/js/runtime-main.aed6ef00.js"
+  }
+]);
